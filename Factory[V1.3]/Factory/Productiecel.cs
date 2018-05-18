6cde2c75f7211bc9f0b9d41552b62c3792b53a5e
basoenema <39376806+basoenema@users.noreply.github.com>
2018-05-18 14:59:40 +0200

    Factory V1.3
    Products disspear and give the treatments that are left and have to be applied to the product

---
+++ Factory[V1.3]/Factory/Productiecel.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Factory
+{
+    public partial class Productiecel : UserControl
+    {
+        public string ProductionCellFunction {
+            get { return LblCellFunction.Text; }
+            set { LblCellFunction.Text = value; }
+        }
+        string _productionCellState;
+        public string ProductionCellState
+        {
+            get { return _productionCellState; }
+            set {
+                _productionCellState = value;
+                LblCellState.Text = value; }
+        }
+        public int ProductionCellProces { get; set; }
+        public bool Available;
+        private Machine _Machine;
+       
+
+        public void SetMachine(Machine m)
+        {
+            if(_Machine != null)
+                 _Machine.PropertyChanged -= MachineStateChanged;
+            _Machine = m;
+            if (m == null) return;
+                 _Machine.PropertyChanged += MachineStateChanged;
+        }
+
+        private void MachineStateChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if(this.InvokeRequired)
+            {
+                this.Invoke(new Action(()=>MachineStateChanged(sender, e)));
+            }
+            if (e.PropertyName != nameof(Machine.State)) return;
+            switch (_Machine.State)
+            {
+                case MachineState.Idle:
+                    BackColor = Color.LightGray;
+                    break;
+                case MachineState.Paused:
+                    BackColor = Color.LightSkyBlue;
+                    break;
+                case MachineState.RequestingProduct:
+                    BackColor = Color.Turquoise;
+                    break;
+                case MachineState.Working:
+                    BackColor = Color.LightGreen;
+                    break;                                     
+            }
+            Invalidate();
+        }
+
+        public Productiecel()
+        {
+            InitializeComponent();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PaintArrow(object sender, PaintEventArgs e)
+        {
+            Control senderPanel = (Control)sender;
+
+            e.Graphics.DrawLine(Pens.Black,
+                new Point(0, senderPanel.Height / 2),
+                new Point(senderPanel.Width, senderPanel.Height / 2)
+                );
+            e.Graphics.DrawLine(Pens.Black,
+                new Point(senderPanel.Width, senderPanel.Height / 2),
+                new Point(senderPanel.Width - 20, senderPanel.Height / 2 - 20)
+                );
+            e.Graphics.DrawLine(Pens.Black,
+                new Point(senderPanel.Width, senderPanel.Height / 2),
+                new Point(senderPanel.Width - 20, senderPanel.Height / 2 + 20)
+
+                );
+        }
+
+        private void Productiecel_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ Factory[V1.3]/Factory/Treatment.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    public class Treatment : IList<Treatment>
+    {
+        public string Name;
+        public List<Treatment> After;
+
+        public int Count => ((IList<Treatment>)After).Count;
+
+        public bool IsReadOnly => ((IList<Treatment>)After).IsReadOnly;
+
+        public Treatment this[int index] { get => ((IList<Treatment>)After)[index]; set => ((IList<Treatment>)After)[index] = value; }
+
+
+       public Treatment(string name)
+        {
+            Name = name;
+            After = new List<Treatment>();
+        }
+
+        public int IndexOf(Treatment item)
+        {
+            return ((IList<Treatment>)After).IndexOf(item);
+        }
+
+        public void Insert(int index, Treatment item)
+        {
+            ((IList<Treatment>)After).Insert(index, item);
+        }
+
+        public void RemoveAt(int index)
+        {
+            ((IList<Treatment>)After).RemoveAt(index);
+        }
+
+        public void Add(Treatment item)
+        {
+            ((IList<Treatment>)After).Add(item);
+        }
+
+        public void Clear()
+        {
+            ((IList<Treatment>)After).Clear();
+        }
+
+        public bool Contains(Treatment item)
+        {
+            return ((IList<Treatment>)After).Contains(item);
+        }
+
+        public void CopyTo(Treatment[] array, int arrayIndex)
+        {
+            ((IList<Treatment>)After).CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(Treatment item)
+        {
+            return ((IList<Treatment>)After).Remove(item);
+        }
+
+        public IEnumerator<Treatment> GetEnumerator()
+        {
+            return ((IList<Treatment>)After).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IList<Treatment>)After).GetEnumerator();
+        }
+    }
+}
